3a10cbd949536b736de06bb44481af1b7e9f6d14
Tuyay <pkschacha64@gmail.com>
2024-11-11 14:33:11 +0100

    Modification du script

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
+#Exo 2 et 3 :
 
-while read -r line
+
+#vérif 1
+if [ $ # -ne 1 ]
+then
+    echo " ce programme demande un argument "
+    exit 1
+fi
+
+FICHIER_URLS = $1
+OK =0
+NOK =0
+
+while read -r LINE ;
 do
-	echo $line
-done < "urls/fr.txt"
+    echo " la ligne : $LINE "
+    if [[ $LINE =∼ ^ https ?:// ]]
+    then
+        echo " ressemble à une URL valide "
+        OK = $ ( expr $OK + 1)
+    else
+        echo " ne ressemble pas à une URL valide "
+        NOK = $ ( expr $NOK + 1)
+    fi
+done
+
+nl -s $'\t' $FICHIER_URLS > ./tableaux/tableau-fr.tsv
+
+
