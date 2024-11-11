a877b313a8cb8076700392721c8d9cf81af48a7f
Tuyay <pkschacha64@gmail.com>
2024-11-11 14:54:34 +0100

    Modification sur script

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -3,29 +3,29 @@
 
 
 #vérif 1
-if [ $ # -ne 1 ]
+if [ $# -ne 1 ]
 then
     echo " ce programme demande un argument "
     exit 1
 fi
 
-FICHIER_URLS = $1
-OK =0
-NOK =0
+FICHIER_URLS=$1
+OK=0
+NOK=0
 
 while read -r LINE ;
 do
-    echo " la ligne : $LINE "
-    if [[ $LINE =∼ ^ https ?:// ]]
+    echo "Ligne : $LINE "
+    if [[ $LINE=~^https?:// ]]
     then
         echo " ressemble à une URL valide "
-        OK = $ ( expr $OK + 1)
+	OK=$((OK + 1))
     else
         echo " ne ressemble pas à une URL valide "
-        NOK = $ ( expr $NOK + 1)
+	NOK=$((NOK + 1))
     fi
-done
+done < $FICHIER_URLS
 
-nl -s $'\t' $FICHIER_URLS > ./tableaux/tableau-fr.tsv
+nl -s $'\t' $FICHIER_URLS > ../tableaux/tableau-fr.tsv
 
 
