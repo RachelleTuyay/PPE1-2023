1f4192bfd60b43781c24f94710b6a96f9ba036e9
Tuyay <pkschacha64@gmail.com>
2024-11-13 11:34:02 +0100

    Correction final du script.

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -2,26 +2,26 @@
 
 if [ $# -ne 1 ]
 then
-    echo " ce programme demande un argument."
+    echo "$0 ce programme demande un argument."
     exit 1
 fi
 
 FICHIER_URLS=$1
 line_nb=1
 
-while read -r LINE
+while read -r line
 do
-    http="footbar"
-    http_code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
-    encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | grep -P -o "charset=\S+")
+    code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
+    encoding=$(curl -o /dev/null -s -I -L -w "%{content_type}" $line | grep -Po "charset=\S+" | cut -d "=" -f2 | tr -d "\r\n")
 
-    encoding=$[encoding:-"N/A"]
+    #encoding=$[ encoding":-N/A" ]
 
     nb_mots=$(lynx -dump -nolist $line | wc -w)
     
-    echo -e "$line_nb\t$LINE\t$encoding\t$nb_mots"
-    line_nb=$(expr $line_nb + 1)
+    echo -e "$line_nb\t$line\t$code\t$encoding\t$nb_mots"
+    line_nb=$((line_nb+1))
     
 done < $FICHIER_URLS
 
 $FICHIER_URLS > ../tableaux/tableau-fr.tsv
+
