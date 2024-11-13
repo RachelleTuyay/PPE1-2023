0bc1072fb6705c42e270083462e897ab02fb4999
Rachelle <156919922+RachelleTuyay@users.noreply.github.com>
2024-11-13 10:20:17 +0100

    Correction miniprojet.sh

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -9,19 +9,19 @@ fi
 FICHIER_URLS=$1
 line_nb=1
 
-while read -r LINE ;
+while read -r LINE
 do
     http="footbar"
-    http_code=$(curl -sILw "%{http_code}" -o /dev/null $line)
-    encoding=$(curl -sILw "%{content_type}" -o /dev/null $line | grep -Po "charset=\S+")
+    http_code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
+    encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | grep -P -o "charset=\S+")
 
     encoding=$[encoding:-"N/A"]
 
     nb_mots=$(lynx -dump -nolist $line | wc -w)
     
-    echo  -e "$line_nb\t$LINE\t$encoding\t$nb_mots"
+    echo -e "$line_nb\t$LINE\t$encoding\t$nb_mots"
     line_nb=$(expr $line_nb + 1)
     
 done < $FICHIER_URLS
 
-$FICHIER_URLS > ../tableaux/tableaux-fr.tsv
+$FICHIER_URLS > ../tableaux/tableau-fr.tsv
