b26bab17671331b36ccc3285f1af015817c01a27
Tuyay <pkschacha64@gmail.com>
2024-11-20 09:28:27 +0100

    Correctoin du script

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -10,16 +10,32 @@ FICHIER_URLS=$1
 line_nb=1
 
 
+echo "<html>
+<head>
+	<meta charset=\"UTF-8\">
+</head>
+
+
+<body>"
+echo "<table>
+<tr>
+	<th>Lignes</th><th>Urls</th>Code<th>Encodage</th><th>Mots</th>
+</tr>
+"
 while read -r line
 do
     code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
     encoding=$(curl -o /dev/null -s -I -L -w "%{content_type}" $line | grep -Po "charset=\S+" | cut -d "=" -f2 | tr -d "\r\n")
 
-    #encoding=$[ encoding":-N/A" ]
+    #encoding=$[ encoding:-"N/A" ]
 
     nb_mots=$(lynx -dump -nolist $line | wc -w)
     
     echo -e "<tr>\n<td>$line_nb</td>\t<td>$line</td>\t<td>$code</td>\t<td>$encoding</td>\t<td>$nb_mots</td>\n</tr>"
     line_nb=$((line_nb+1))
     
-done < $FICHIER_URLS > tableau-fr.html
+done < $FICHIER_URLS > ../tableaux/tableau-fr.html
+
+echo "</table>"
+echo "</body>
+</html>"
